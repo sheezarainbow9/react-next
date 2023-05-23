17eb45992f58379d71175421f12d5a440001e833
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-05-23 15:29:11 -0300

    componentes com estado

--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ImagemAleatoria() {
-  let pesquisa: string = "";
-  const url = "https://source.unsplash.com/featured/300x300?";
-
-  function urlImagem() {
-    return `${url}${pesquisa}`;
-  }
+  const [pesquisa, alterarPesquisa] = useState<string>("abstract");
+  const [tamanho, alterarTamanho] = useState<number>(300);
+  const url = "https://source.unsplash.com/featured/";
 
   function renderizarBotao(valor: string) {
     return (
@@ -15,8 +13,8 @@ export default function ImagemAleatoria() {
       bg-blue-500 px-4 py-2 rounded-md
     `}
         onClick={() => {
-          pesquisa = valor;
-          console.log(urlImagem());
+          alterarPesquisa(valor);
+          console.log(valor);
         }}
       >
         {valor}
@@ -26,11 +24,32 @@ export default function ImagemAleatoria() {
 
   return (
     <div className="flex flex-col gap-3 border border-zinc-500 p-7 rounded-md">
-      <Image src={urlImagem()} height={300} width={300} alt="Imagem" />
+      <div className="flex justify-center gap-7 mb-5">
+        <span>{pesquisa}</span>
+        <span>{tamanho}x{tamanho}</span>
+      </div>
+      <Image
+        src={`${url}${tamanho}x${tamanho}?${pesquisa}`}
+        height={300}
+        width={300}
+        alt="Imagem"
+        className="rounded-md"
+      />
       <div className="flex gap-5">
         {renderizarBotao("abstract")}
         {renderizarBotao("city")}
         {renderizarBotao("person")}
+      </div>
+      <div>
+        <input
+          type="number"
+          value={tamanho}
+          className="bg-zinc-800 p-2 rounded-md w-full"
+          outline-none
+          onChange={e => {
+            alterarTamanho(+e.target.value)
+          }}
+        />
       </div>
     </div>
   );
